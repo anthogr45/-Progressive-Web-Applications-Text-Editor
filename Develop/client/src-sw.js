c764ccb0746d6166bbfddaa111533376bb93d140
AnthoG <anthony.dswg@gmail.com>
2024-01-03 14:28:51 -0500

    Some Errors Fixed

--- Develop/client/src-sw.js
+++ Develop/client/src-sw.js
@@ -1,14 +1,63 @@
-const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
-const { registerRoute } = require('workbox-routing');
-const { CacheableResponsePlugin } = require('workbox-cacheable-response');
-const { ExpirationPlugin } = require('workbox-expiration');
-const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+// const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
+// const { CacheFirst } = require('workbox-strategies');
+// const { registerRoute } = require('workbox-routing');
+// const { CacheableResponsePlugin } = require('workbox-cacheable-response');
+// const { ExpirationPlugin } = require('workbox-expiration');
+// const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
 
+// precacheAndRoute(self.__WB_MANIFEST);
+
+// const pageCache = new CacheFirst({
+//   cacheName: 'page-cache',
+//   plugins: [
+//     new CacheableResponsePlugin({
+//       statuses: [0, 200],
+//     }),
+//     new ExpirationPlugin({
+//       maxAgeSeconds: 30 * 24 * 60 * 60,
+//     }),
+//   ],
+// });
+
+// warmStrategyCache({
+//   urls: ['/index.html', '/'],
+//   strategy: pageCache,
+// });
+
+// registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+
+// // TODO: Implement asset caching
+
+// registerRoute(
+//   ({ request }) => {
+//     console.log(request);
+//     return (
+//       // CSS
+//       request.destination === 'style' ||
+//       // JavaScript
+//       request.destination === 'script'
+//     );
+//   },
+//   new StaleWhileRevalidate({
+//     cacheName: 'static-resources',
+//     plugins: [
+//       new CacheableResponsePlugin({
+//         statuses: [0, 200],
+//       }),
+//     ],
+//   })
+// );
+
+const { warmStrategyCache } = require("workbox-recipes");
+const { CacheFirst, StaleWhileRevalidate } = require("workbox-strategies");
+const { registerRoute } = require("workbox-routing");
+const { CacheableResponsePlugin } = require("workbox-cacheable-response");
+const { ExpirationPlugin } = require("workbox-expiration");
+const { precacheAndRoute } = require("workbox-precaching/precacheAndRoute");
 precacheAndRoute(self.__WB_MANIFEST);
-
+// Set up page cache
 const pageCache = new CacheFirst({
-  cacheName: 'page-cache',
+  cacheName: "page-cache",
   plugins: [
     new CacheableResponsePlugin({
       statuses: [0, 200],
@@ -18,28 +67,16 @@ const pageCache = new CacheFirst({
     }),
   ],
 });
-
 warmStrategyCache({
-  urls: ['/index.html', '/'],
+  urls: ["/index.html", "/"],
   strategy: pageCache,
 });
-
-registerRoute(({ request }) => request.mode === 'navigate', pageCache);
-
-// TODO: Implement asset caching
-
+registerRoute(({ request }) => request.mode === "navigate", pageCache);
+// Set up asset cache
 registerRoute(
-  ({ request }) => {
-    console.log(request);
-    return (
-      // CSS
-      request.destination === 'style' ||
-      // JavaScript
-      request.destination === 'script'
-    );
-  },
+  ({ request }) => ["style", "script", "worker"].includes(request.destination),
   new StaleWhileRevalidate({
-    cacheName: 'static-resources',
+    cacheName: "asset-cache",
     plugins: [
       new CacheableResponsePlugin({
         statuses: [0, 200],
@@ -48,46 +85,5 @@ registerRoute(
   })
 );
 
-// // Register the service worker
-// if ('serviceWorker' in navigator) {
-//   window.addEventListener('load', () => {
-//     navigator.serviceWorker.register('/service-worker.js')
-//       .then(registration => {
-//         console.log('Service Worker registered with scope:', registration.scope);
-//       })
-//       .catch(error => {
-//         console.log('Service Worker registration failed:', error);
-//       });
-//   });
-// }
 
-// // Service Worker script (service-worker.js)
-// self.addEventListener('install', event => {
-//   event.waitUntil(
-//     caches.open('assets-cache')
-//       .then(cache => {
-//         return cache.addAll([
-//           '/',
-//           '/index.html',
-//           '/styles.css',
-//           '/script.js',
-//           '/images/logo.png',
-//           // Add other assets to cache here
-//         ]);
-//       })
-//   );
-// });
-
-// self.addEventListener('fetch', event => {
-//   event.respondWith(
-//     caches.match(event.request)
-//       .then(response => {
-//         if (response) {
-//           return response;
-//         }
-
-//         return fetch(event.request);
-//       })
-//   );
-// });
 registerRoute();

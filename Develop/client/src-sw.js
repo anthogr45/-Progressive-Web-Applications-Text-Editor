d310e647bf39ad9913a33565a86b923bf0dbc020
AnthoG <anthony.dswg@gmail.com>
2024-01-03 17:33:52 -0500

    Some Errors Fixed33

--- Develop/client/src-sw.js
+++ Develop/client/src-sw.js
@@ -86,4 +86,22 @@ registerRoute(
 );
 
 
-registerRoute();
+registerRoute(
+  ({ request }) => request.destination === 'image',
+  new CacheFirst({
+    cacheName: 'my-image-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+      }),
+    ],
+  })
+);
+
+
+
+// registerRoute();

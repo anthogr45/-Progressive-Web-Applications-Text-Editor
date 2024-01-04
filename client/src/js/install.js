67bbca8221ecbb4935b3d006831db50305e7f672
AnthoG <anthony.dswg@gmail.com>
2024-01-04 00:29:01 -0500

    Install.js isuue fixes and folder structure changes

---
+++ client/src/js/install.js
@@ -0,0 +1,29 @@
+const butInstall = document.getElementById('buttonInstall');
+// const textHeader = document.getElementById('textHeader');
+// let deferredPrompt;
+
+// Logic for installing the PWA
+// TODO: Add an event handler to the `beforeinstallprompt` event
+window.addEventListener('beforeinstallprompt', (event) => {
+
+  console.log('beforeinstallprompt event triggered');
+  window.deferredPrompt = event;
+  butInstall.style.visibility = 'visible';
+  // textHeader.textContent = 'Click the button to install!';
+});
+
+// TODO: Implement a click event handler on the `butInstall` element
+butInstall.addEventListener('click', async () => {
+let deferredPrompt = window.deferredPrompt
+  deferredPrompt.prompt();
+  window.deferredPrompt = null;
+  butInstall.setAttribute('disabled', true);
+  butInstall.textContent = 'Installed!';
+});
+
+// TODO: Add a handler for the `appinstalled` event
+window.addEventListener('appinstalled', (event) => {
+  window.deferredPrompt = null;
+  textHeader.textContent = 'Successfully installed!';
+  console.log('👍', 'appinstalled', event);
+});
